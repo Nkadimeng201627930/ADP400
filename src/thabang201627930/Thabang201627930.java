a39dd210db1c71deb10615de1f7dc6398f94ac72
219020140 <219020140@MBO_ICT102_09-PC.ump.ac.za>
2020-08-24 01:39:04 +0200

    My First Commit

---
+++ src/thabang201627930/Thabang201627930.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package thabang201627930;
+
+/**
+ *
+ * @author 219020140
+ */
+public class Thabang201627930 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         int a=5, b=4;
+        int sum=a+b;
+        System.out.println("The sum of the two numbers is equal to :"+sum);
+    }
+    
+}
